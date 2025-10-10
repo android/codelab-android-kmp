a2c36977e8bf86d103af2bc99097703c1abdc408
shuja1497 <shujareshi1497@gmail.com>
2025-10-10 09:57:27 +0530

    Integrate SKIE

--- get-started/build.gradle.kts
+++ get-started/build.gradle.kts
@@ -25,6 +25,7 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.androidKotlinMultiplatformLibrary) apply false
     alias(libs.plugins.androidLint) apply false
+    alias(libs.plugins.skie) apply false
 }
 
 subprojects {
--- get-started/iosApp/KMPGetStartedCodelab/ContentView.swift
+++ get-started/iosApp/KMPGetStartedCodelab/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, \(Platform_iosKt.platform())!")
+            Text("Hello, \(platform())!")
         }
         .padding()
     }
